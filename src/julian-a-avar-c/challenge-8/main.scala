a3c54941b93e29e7f7fae3e63dce7989988983db
Julian A. Avar C. <dev@julian-a-avar-c.me>
2024-01-29 21:10:38 -0500

    moved folders around

---
+++ src/julian-a-avar-c/challenge-8/main.scala
@@ -0,0 +1,9 @@
+//> using dep org.processing:core:3.3.7
+//> using dep org.processing:net:3.3.7
+//> using dep org.processing:video:3.3.7
+//> using dep org.processing:serial:3.3.7
+//> using dep org.processing:pde:3.3.7
+//> using dep org.processing:pdf:3.3.7
+
+@main def main() =
+    println("Hello World")
